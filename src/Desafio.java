e1c9722c4565eabf407d9c345dfb06eb79ba08b4
Adilson Luiz de Sousa Junior <154681459+Adilson141192@users.noreply.github.com>
2025-03-03 11:27:38 -0300

    030325 aula0404

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,5 +1,30 @@
-public class Main {
+import java.util.Scanner;
+
+public class Desafio {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String nome = "Clark Kent";
+        String tipoConta = "Conta Corrente";
+        double saldo = 1599.99;
+        int opcao = 0;
+
+        System.out.println("******************************");
+        System.out.println("\nNome do Cliente: " + nome);
+        System.out.println("Tipo da Conta: " + tipoConta);
+        System.out.println("Saldo atual: " + saldo);
+        System.out.println("\n******************************");
+
+        String menu = """
+                ** Digite a sua opção: **
+                1 - Consultar saldo.
+                2 - Transferir valor.
+                3 - Receber valor.
+                4 - Sair.
+                """;
+        Scanner leitura = new Scanner(System.in);
+
+        while (opcao != 4) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+        }
     }
 }
